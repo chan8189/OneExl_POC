5fc582488b801970e5d7922af60798d3a52ed733
Chandan Choudhary <chaudhary.chandan48@gmail.com>
2020-04-13 08:18:05 +0530

    feedback design 1

--- lib/screens/feedback.dart
+++ lib/screens/feedback.dart
@@ -5,7 +5,45 @@ class Feedback1 extends StatefulWidget {
   _Feedback1State createState() => _Feedback1State();
 }
 
-class _Feedback1State extends State<Feedback1> with AutomaticKeepAliveClientMixin<Feedback1>{
+class Item {
+  const Item(this.name, this.iconUp);
+
+  final String name;
+
+  //final Icon icon;
+  final ImageIcon iconUp;
+}
+
+class _Feedback1State extends State<Feedback1>
+    with AutomaticKeepAliveClientMixin<Feedback1> {
+  Item selectedUser;
+  List<Item> users = <Item>[
+    const Item(
+        'eVoyage',
+        ImageIcon(
+          AssetImage('images/logo.png'),
+          size: 50,
+        )),
+    const Item(
+        'OneExl',
+        ImageIcon(
+          AssetImage('images/logo.png'),
+          size: 50,
+        )),
+    const Item(
+        'iExpense',
+        ImageIcon(
+          AssetImage('images/logo.png'),
+          size: 50,
+        )),
+    const Item(
+        'RTP',
+        ImageIcon(
+          AssetImage('images/logo.png'),
+          size: 50,
+        )),
+  ];
+
   @override
   Widget build(BuildContext context) {
     print('build About Us');
@@ -16,11 +54,72 @@ class _Feedback1State extends State<Feedback1> with AutomaticKeepAliveClientMixi
           IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Feedback',
-          style: TextStyle(fontSize: 30),
-        ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.fromLTRB(10, 10, 0.0, 0.0),
+              child: Text(
+                "Share your app experiance with us",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.0,
+                  wordSpacing: 3.0,
+                  fontSize: 16.0,
+                ),
+              )),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            width: 380.0,
+            height: 50.0,
+            color: Colors.grey[200],
+            margin: EdgeInsets.fromLTRB(10, 0.0, 0.0, 10),
+            child: DropdownButton<Item>(
+              hint: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  "Select Application",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              value: selectedUser,
+              icon: Icon(Icons.arrow_drop_down),
+              elevation: 16,
+              isDense: false,
+              underline: SizedBox(),
+              onChanged: (Item Value) {
+                setState(() {
+                  selectedUser = Value;
+                });
+              },
+              items: users.map((Item user) {
+                return DropdownMenuItem<Item>(
+                  value: user,
+                  child: Row(
+                    children: <Widget>[
+                      user.iconUp,
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        user.name,
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          SizedBox(height: 200)
+        ],
       ),
     );
   }
